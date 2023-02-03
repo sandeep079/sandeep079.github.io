12786ccbdfce4ea023fe054b71623b3a4ba8da35
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2023-02-03 06:39:30 +0545

    Added new projects

--- src/components/experience/Data.jsx
+++ src/components/experience/Data.jsx
@@ -1,211 +1,336 @@
-import osd from '../../assets/osd.jpg';
-import Experience3 from '../../assets/33.png';
-import anitaB from '../../assets/anitaB.jpg';
-import gfd from '../../assets/gfd.jpg';
-import git from '../../assets/git.jpg';
-import sff from '../../assets/sff.jpg';
-import pg from '../../assets/pg.png';
-import zerone from '../../assets/zerone.png';
-import weCode from '../../assets/weCode.png';
-import tu from '../../assets/tu.png';
-import hmv from '../../assets/hmv.png';
-import swarnim from '../../assets/swarnim.png';
-import kmc from '../../assets/kmc.jpg';
-import codeRush from '../../assets/codeRush.png';
-import aceIgnite from '../../assets/aceIgnite.png';
-
+import osd from "../../assets/osd.jpg";
+import Experience3 from "../../assets/33.png";
+import anitaB from "../../assets/anitaB.jpg";
+import gfd from "../../assets/gfd.jpg";
+import git from "../../assets/git.jpg";
+import sff from "../../assets/sff.jpg";
+import pg from "../../assets/pg.png";
+import zerone from "../../assets/zerone.png";
+import weCode from "../../assets/weCode.png";
+import tu from "../../assets/tu.png";
+import hmv from "../../assets/hmv.png";
+import swarnim from "../../assets/swarnim.png";
+import kmc from "../../assets/kmc.jpg";
+import codeRush from "../../assets/codeRush.png";
+import aceIgnite from "../../assets/aceIgnite.png";
+import revampathon from "../../assets/revampathon.jpg";
+import codecamp from "../../assets/codecamp.png";
 
 export const technicalData = [
-    {
-        image: pg,
-        title: 'Technical Manager',
-        company: 'Pulchowk Girls',
-        date: 'Jun 2022 - Present',
-        description: (
-            <ul className='ul'>
-                <li>Manage technical aspects and design posters of the events.</li>
-            </ul>),
-    },
-    {
-        image: Experience3,
-        title: 'Instructor',
-        company: 'Workshops & Traning Programs',
-        date: '',
-        description: (
-            <ul className='ul'>
-                <li><b>Version Control System with Git&emsp;</b>
-                |&emsp;<button>Jul 12 - Jul 15, 2022</button><br />
-                Instructed students on git and version control in a 4-day workshop organized by IT Club, Pulchowk.</li>
-                <li><b>Microsoft Bootcamp Trainer, Girls in Tech–Nepal&emsp;</b>
-                |&emsp;<button>Apr - Jun, 2022</button><br />
-                Instructor for Microsoft Office training given to students of Center for Disabled Children Assistance (CDCA), Dhanyawaad, and Tara Namaste Orphanage.</li>
-            </ul>),
-    },
-    {
-        image: osd,
-        title: 'Open Source Day Mentor, Participant',
-        company: 'AnitaB.org',
-        date: 'Jul 2021 | Sep 16, 2022',
-        description: (
-            <ul className='ul'>
-                <li><b>Open Source Day 2021 (Mentor)&emsp;</b>
-                |&emsp;<button>Jul, 2021</button><br />
-                Mentored participants on using git, GitHub, setting up the project <a href='https://github.com/smaranjitghose/awesome-portfolio-websites' target='_blank'>Awesome Portfolio Websites</a> and helped them make their first open source contributions.</li>
-                <li><b>Open Source Day 2022 (GHC Scholar)&emsp;</b>
-                |&emsp;<button>Sep 16, 2022</button><br />
-                Contributed to <a href='https://github.com/numpy/numpy' target='_blank'>NumPy</a> documentation.</li>
-            </ul>),
-    },
-    
-    {
-        image: codeRush,
-        title: 'Code Like Her Fellowship',
-        company: 'Code Rush',
-        date: 'Dec 2021 - Mar 2022',
-        description: (
-            <ul className='ul'>
-                <li>Selected as one of the 15 fellows.</li>
-                <li><b>Skills:</b> HTML, CSS, JavaScript, NodeJS, Express, React.js</li>
-                <li><b>Projects:</b> <a href='https://github.com/Coderushnepal/YunikaBajracharya' target='_blank'>Click Here </a>
-                &#x2B29;
-                <a href='https://medium.com/code-rush-blog/code-like-her-fellowship-ii-weekly-6-32ee89b54cc1'> Blog</a>
-                </li>
-            </ul>),
-    },
-    {
-        image: sff,
-        title: 'Seeds for the Future 2021',
-        company: 'Huawei',
-        date: 'Nov - Dec 2021',
-        description: (
-            <ul className='ul'>
-                <li>Led the team project ‘Tech4Good’ on tracking the insulin dose and glucometer readings for Diabetic patients.</li>
-                <li>Learned fundamental courses on 5G, Cloud Computing, and AI</li>
-                <li>Visited 4G base station in Ghandruk and attended sessions on leadership, tech talks, cultural exchange.</li>
-                <li>Project Details: 
-                    <a href='https://sathi-app.github.io/' target='_blank'> Website </a>&#x2B29;
-                    <a href='https://www.youtube.com/watch?v=mxnGCWSWLTY&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4' target='_blank'> Intro Video </a>&#x2B29;
-                    <a href='https://www.youtube.com/watch?v=ij6_imqs3Nc&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4' target='_blank'> Demo Video</a>
-                </li>
-                <li><a href='https://www.youtube.com/watch?v=k4MR0XnlJ_I&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=1' target='_blank'>Student Representative</a></li>
-            </ul>),
-    },
+  {
+    image: pg,
+    title: "Technical Manager",
+    company: "Pulchowk Girls",
+    date: "Jun 2022 - Present",
+    description: (
+      <ul className="ul">
+        <li>Manage technical aspects and design posters of the events.</li>
+      </ul>
+    ),
+  },
+  {
+    image: Experience3,
+    title: "Instructor",
+    company: "Workshops & Traning Programs",
+    date: "",
+    description: (
+      <ul className="ul">
+        <li>
+          <b>Version Control System with Git&emsp;</b>
+          |&emsp;<button>Jul 12 - Jul 15, 2022</button>
+          <br />
+          Instructed students on git and version control in a 4-day workshop
+          organized by IT Club, Pulchowk.
+        </li>
+        <li>
+          <b>Microsoft Bootcamp Trainer, Girls in Tech–Nepal&emsp;</b>
+          |&emsp;<button>Apr - Jun, 2022</button>
+          <br />
+          Instructor for Microsoft Office training given to students of Center
+          for Disabled Children Assistance (CDCA), Dhanyawaad, and Tara Namaste
+          Orphanage.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    image: osd,
+    title: "Open Source Day Mentor, Participant",
+    company: "AnitaB.org",
+    date: "Jul 2021 | Sep 16, 2022",
+    description: (
+      <ul className="ul">
+        <li>
+          <b>Open Source Day 2021 (Mentor)&emsp;</b>
+          |&emsp;<button>Jul, 2021</button>
+          <br />
+          Mentored participants on using git, GitHub, setting up the project{" "}
+          <a
+            href="https://github.com/smaranjitghose/awesome-portfolio-websites"
+            target="_blank"
+          >
+            Awesome Portfolio Websites
+          </a>{" "}
+          and helped them make their first open source contributions.
+        </li>
+        <li>
+          <b>Open Source Day 2022 (GHC Scholar)&emsp;</b>
+          |&emsp;<button>Sep 16, 2022</button>
+          <br />
+          Contributed to{" "}
+          <a href="https://github.com/numpy/numpy" target="_blank">
+            NumPy
+          </a>{" "}
+          documentation.
+        </li>
+      </ul>
+    ),
+  },
+
+  {
+    image: codeRush,
+    title: "Code Like Her Fellowship",
+    company: "Code Rush",
+    date: "Dec 2021 - Mar 2022",
+    description: (
+      <ul className="ul">
+        <li>Selected as one of the 15 fellows.</li>
+        <li>
+          <b>Skills:</b> HTML, CSS, JavaScript, NodeJS, Express, React.js
+        </li>
+        <li>
+          <b>Projects:</b>{" "}
+          <a
+            href="https://github.com/Coderushnepal/YunikaBajracharya"
+            target="_blank"
+          >
+            Click Here{" "}
+          </a>
+          &#x2B29;
+          <a href="https://medium.com/code-rush-blog/code-like-her-fellowship-ii-weekly-6-32ee89b54cc1">
+            {" "}
+            Blog
+          </a>
+        </li>
+      </ul>
+    ),
+  },
+  {
+    image: sff,
+    title: "Seeds for the Future 2021",
+    company: "Huawei",
+    date: "Nov - Dec 2021",
+    description: (
+      <ul className="ul">
+        <li>
+          Led the team project ‘Tech4Good’ on tracking the insulin dose and
+          glucometer readings for Diabetic patients.
+        </li>
+        <li>Learned fundamental courses on 5G, Cloud Computing, and AI</li>
+        <li>
+          Visited 4G base station in Ghandruk and attended sessions on
+          leadership, tech talks, cultural exchange.
+        </li>
+        <li>
+          Project Details:
+          <a href="https://sathi-app.github.io/" target="_blank">
+            {" "}
+            Website{" "}
+          </a>
+          &#x2B29;
+          <a
+            href="https://www.youtube.com/watch?v=mxnGCWSWLTY&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4"
+            target="_blank"
+          >
+            {" "}
+            Intro Video{" "}
+          </a>
+          &#x2B29;
+          <a
+            href="https://www.youtube.com/watch?v=ij6_imqs3Nc&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4"
+            target="_blank"
+          >
+            {" "}
+            Demo Video
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/watch?v=k4MR0XnlJ_I&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=1"
+            target="_blank"
+          >
+            Student Representative
+          </a>
+        </li>
+      </ul>
+    ),
+  },
 ];
 
 export const experienceData = [
-    {
-        image: zerone,
-        title: 'Editor',
-        company: 'The Zerone',
-        date: 'Jan 2020 - Present',
-        description: (
-            <ul className='ul'>
-                <li><b>Associate Editor&emsp;</b>
-                |&emsp;<button>The Zerone 2021</button><br />
-                Led the team for grammar-related social media posts, wrote and edited articles for the magazine.</li>
-                <li><b>Editor&emsp;</b>
-                |&emsp;<button>The Zerone 2020</button><br />
-                Wrote, edited articles, and designed posters for the magazine, writing sessions, and competitions.</li>
-            </ul>),
-    },
-    {
-        image: gfd,
-        title: 'Communication and Outreach Lead',
-        company: 'GitHub Field Day Nepal',
-        date: 'Apr - Jun 2022',
-        description: (
-            <>
-            GitHub Field Day is an unconference for leaders of technical student communities. Field day Nepal 2022 brought together 50 student leaders from 12 cities representing 40+ tech communities.
-            <br /><br />
-            <ul className='ul-small-gap'>
-                
-                <li>One of the organizers for the first GitHub Field Day Nepal.</li>
-                <li>Reached out to 50+ tech communities, media channels and IT colleges across the country.</li>
-                <li>Responsible for communication with keynote speaker, campus experts and participants.</li>
-                <li>Moderated Group Discussions during the Field Day.</li>
-            </ul>
-            </>
-        ),
-    },
-    {
-        image: git,
-        title: 'Outreach Coordinator Ambassador',
-        company: 'Girls in Tech-Nepal',
-        date: 'Jan 2021 - Present',
-        description: (<>
-        <ul className='ul-small-gap'>   
-            <li>Reached out to speakers for <a href='https://www.youtube.com/watch?v=nKxwriaQxO0&list=PLrC-5k2StDOiqFW3ssT_tnStQG88SYVNW&index=4' target='_blank'>Her Journey</a> series and webinars.</li>
-            <li>Moderated webinars and promote GIT-Nepal.</li>
-            <li>Trainer for Microsoft Office Bootcamp</li>
+  {
+    image: zerone,
+    title: "Editor",
+    company: "The Zerone",
+    date: "Jan 2020 - Present",
+    description: (
+      <ul className="ul">
+        <li>
+          <b>Associate Editor&emsp;</b>
+          |&emsp;<button>The Zerone 2021</button>
+          <br />
+          Led the team for grammar-related social media posts, wrote and edited
+          articles for the magazine.
+        </li>
+        <li>
+          <b>Editor&emsp;</b>
+          |&emsp;<button>The Zerone 2020</button>
+          <br />
+          Wrote, edited articles, and designed posters for the magazine, writing
+          sessions, and competitions.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    image: gfd,
+    title: "Communication and Outreach Lead",
+    company: "GitHub Field Day Nepal",
+    date: "Apr - Jun 2022",
+    description: (
+      <>
+        GitHub Field Day is an unconference for leaders of technical student
+        communities. Field day Nepal 2022 brought together 50 student leaders
+        from 12 cities representing 40+ tech communities.
+        <br />
+        <br />
+        <ul className="ul-small-gap">
+          <li>One of the organizers for the first GitHub Field Day Nepal.</li>
+          <li>
+            Reached out to 50+ tech communities, media channels and IT colleges
+            across the country.
+          </li>
+          <li>
+            Responsible for communication with keynote speaker, campus experts
+            and participants.
+          </li>
+          <li>Moderated Group Discussions during the Field Day.</li>
         </ul>
-        </>),
-    },
-    {
-        image: hmv,
-        title: 'Online Women Empowerment Program',
-        company: "Hear My Voice - Women's Network",
-        date: 'May - Jun 2020',
-        description: (
-            <ul className='ul'>
-                <li>Discussed women's rights and social justice with women across 7 countries for 6 weeks.</li>
-                <li>Panelist in Panel Discussion - ‘Stand Up Against Gender-Based Violence’ on Nov 23, 2020 to celebrate the beginning of the 16 Days of Activism against Gender-Based Violence.</li>
-            </ul>),
-    },
+      </>
+    ),
+  },
+  {
+    image: git,
+    title: "Outreach Coordinator Ambassador",
+    company: "Girls in Tech-Nepal",
+    date: "Jan 2021 - Present",
+    description: (
+      <>
+        <ul className="ul-small-gap">
+          <li>
+            Reached out to speakers for{" "}
+            <a
+              href="https://www.youtube.com/watch?v=nKxwriaQxO0&list=PLrC-5k2StDOiqFW3ssT_tnStQG88SYVNW&index=4"
+              target="_blank"
+            >
+              Her Journey
+            </a>{" "}
+            series and webinars.
+          </li>
+          <li>Moderated webinars and promote GIT-Nepal.</li>
+          <li>Trainer for Microsoft Office Bootcamp</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    image: hmv,
+    title: "Online Women Empowerment Program",
+    company: "Hear My Voice - Women's Network",
+    date: "May - Jun 2020",
+    description: (
+      <ul className="ul">
+        <li>
+          Discussed women's rights and social justice with women across 7
+          countries for 6 weeks.
+        </li>
+        <li>
+          Panelist in Panel Discussion - ‘Stand Up Against Gender-Based
+          Violence’ on Nov 23, 2020 to celebrate the beginning of the 16 Days of
+          Activism against Gender-Based Violence.
+        </li>
+      </ul>
+    ),
+  },
 ];
 
 export const scholarshipData = [
-    {
-        image: aceIgnite,
-        title: 'Ace Ignite 48-hour Hackathon 2022 Winner',
-        company: 'Ace Institute of Management',
-        date: 'Nov 11-13, 2022',
-        description:'',
-    },
-    {
-        image: anitaB,
-        title: 'Grace Hopper Celebration Scholar 2022',
-        company: 'AnitaB.org',
-        date: '',
-        description:'',
-    },
-    {
-        image: weCode,
-        title: 'Harvard WECode Scholar 2022',
-        company: 'Harvard WECode',
-        date: '',
-        description: '',
-    },
+  {
+    image: codecamp,
+    title: "UXCam Code Camp First Runner Up",
+    company: "Locus 2023",
+    date: "Jan 28-29, 2023",
+    description: "",
+  },
+  {
+    image: revampathon,
+    title: "Revampathon Winner",
+    company: "Leapfrog Student Partnership Program",
+    date: "Dec 2-17, 2022",
+    description: "",
+  },
+  {
+    image: aceIgnite,
+    title: "Ace Ignite 48-hour Hackathon 2022 Winner",
+    company: "Ace Institute of Management",
+    date: "Nov 11-13, 2022",
+    description: "",
+  },
+  {
+    image: anitaB,
+    title: "Grace Hopper Celebration Scholar 2022",
+    company: "AnitaB.org",
+    date: "",
+    description: "",
+  },
+  {
+    image: weCode,
+    title: "Harvard WECode Scholar 2022",
+    company: "Harvard WECode",
+    date: "",
+    description: "",
+  },
 ];
 
 export const educationData = [
-    {
-        image: tu,
-        title: 'Tribhuvan Universtiy, Pulchowk Campus',
-        company: 'Bachelor in Computer Engineering ',
-        date: ' | 2019-2024',
-        description: '',
-    },
-    {
-        image: kmc,
-        title: 'Kathmandu Model Secondary School',
-        company: 'Grade 11, 12 ',
-        date: ' | 2017-2018',
-        description:(
-            <ul>
-                <li>&#x2B29; President, KMC Art Club 2018-19</li>
-                <li>&#x2B29; Winner, KMC Annual Art Competition 2019</li>
-            </ul>
-        ),
-    },
-    {
-        image: swarnim,
-        title: 'Swarnim School',
-        company: 'Grade 1-10 ',
-        date: ' | Graduated: 2016',
-        description: (
-            <ul>
-                <li>&#x2B29; Head Girl, 22nd Batch</li>
-            </ul>
-        ),
-    },
+  {
+    image: tu,
+    title: "Tribhuvan Universtiy, Pulchowk Campus",
+    company: "Bachelor in Computer Engineering ",
+    date: " | 2019-2024",
+    description: "",
+  },
+  {
+    image: kmc,
+    title: "Kathmandu Model Secondary School",
+    company: "Grade 11, 12 ",
+    date: " | 2017-2018",
+    description: (
+      <ul>
+        <li>&#x2B29; President, KMC Art Club 2018-19</li>
+        <li>&#x2B29; Winner, KMC Annual Art Competition 2019</li>
+      </ul>
+    ),
+  },
+  {
+    image: swarnim,
+    title: "Swarnim School",
+    company: "Grade 1-10 ",
+    date: " | Graduated: 2016",
+    description: (
+      <ul>
+        <li>&#x2B29; Head Girl, 22nd Batch</li>
+      </ul>
+    ),
+  },
 ];
--- src/components/project/Data.jsx
+++ src/components/project/Data.jsx
@@ -5,76 +5,129 @@ import clhp from "../../assets/clhp.png";
 import sathi from "../../assets/sathi.png";
 import maitrika from "../../assets/maitrika.png";
 import sahayatri from "../../assets/sahayatri.png";
+import newsly from "../../assets/newsly.png";
 
 export const Data = [
-    {
-        image: sahayatri,
-        title: 'SahaYatri',
-        category: '',
-        link: 'https://github.com/YAARS-2022',
-        demo: 'https://www.youtube.com/watch?v=Xf5o13Zutak',
-        description: 'Real-time school bus tracking system using a low-powered GPS tracking device, with a web app for schools to monitor their school buses, and mobile app for parents to know their child’s location.',
-        stack: ['React', 'Flutter', 'Firebase','Open Street Map','Lora', 'Arduino, NodeMCU'],
-    },
-    {
-        image: maitrika,
-        title: 'Maitrika',
-        category: '',
-        link: 'https://github.com/Yunika-Bajracharya/Hungry-Hackee-Hunts',
-        demo: '',
-        description: ' Application that connects health workers with doctors, tracks events, and generates reports based on data taken by health workers to aid in checkups.',
-        stack: ['MERN stack', 'WebRTC', 'i18next','chart.js'],
-    },
-    {
-        image: hhh,
-        title: 'Hungry Hackee Hunts',
-        category: '',
-        link: 'https://github.com/Yunika-Bajracharya/Hungry-Hackee-Hunts',
-        demo: 'https://www.yunikabajracharya.com.np/Hungry-Hackee-Hunts/',
-        description: 'A game in which you help Hackee, the squirrel, in her walnut heist while visualizing different sorting algorithms and a maze generator created using recursive depth first search. There are two levels.',
-        stack: ['JavaScript', 'HTML', 'CSS','Canva'],
-    },
-    {
-        image: chess,
-        title: 'Chess',
-        category: '',
-        link: 'https://github.com/Yunika-Bajracharya/Chess',
-        demo: '',
-        description: 'A chess game with features such as timer, single/multi-player mode, scoreboard, and resign options, created using C++ and SDL2 for GUI.',
-        stack: ['C++', 'SDL2', 'CMAKE'],
-    },
-    {
-        image: clhp,
-        title: 'Code Like Her Fellowship Projects',
-        category: '',
-        link: 'https://github.com/Coderushnepal/YunikaBajracharya',
-        demo: '',
-        description: 'Here is the list of projects and assignments I did during the fellowship.',
-        stack: ['HTML', 'CSS', 'JavaScript', 'Express'],
-    },
-    {
-        image: dhungedhara,
-        title: 'Dhungedhara',
-        category: '',
-        link: 'https://github.com/Yunika-Bajracharya/Dhungedhara',
-        demo: '',
-        description: "This is a simulation of a traditional stone drinking water fountain called Lho Hiti. The 3D model also includes the surrounding, flowers, stairs and water jar called 'gagri' in Nepali with the day time as well as night time views.",
-        stack: ["C++", 'OpenGL', 'GLSL', 'Blender'],
-    },
-    {
-        image: sathi,
-        title: 'Sathi',
-        category: '',
-        link: '',
-        demo: 'https://sathi-app.github.io/',
-        description:  (
-            <ul>
-                <li>Sathi is a friendly app for Type I Diabetic Patients to make their life easier. It sends notification to the patient regarding timely blood sugar checkup and medication, to the trusted person if the patient misses the notification, and  to the doctor if the patient's blood sugar level goes extremely high.</li><br />
-                <li> Project Details: 
-                    <a href='https://www.youtube.com/watch?v=mxnGCWSWLTY&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4' target='_blank'> Intro Video </a>&#x2B29;
-                    <a href='https://www.youtube.com/watch?v=ij6_imqs3Nc&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4' target='_blank'> Demo Video</a>
-                </li>
-            </ul>),
-        stack: ["Seeds For the Future 2021 Project"],
-    },
-]
+  {
+    image: newsly,
+    title: "Newsly",
+    category: "",
+    link: "https://github.com/Yunika-Bajracharya/Newsly_frontend",
+    demo: "",
+    description:
+      "Newsly provides summarized news stories and the day's top news that can be read or listened in either English or Nepali. It also offers a daily newsletter tailored to the user's preferences through email and real-time news update in Discord and Telegram.",
+    stack: [
+      "Flutter",
+      "Django",
+      "IBM Watson",
+      "OpenAI",
+      "Google Text-to-Speech",
+    ],
+  },
+  {
+    image: sahayatri,
+    title: "SahaYatri",
+    category: "",
+    link: "https://github.com/YAARS-2022",
+    demo: "https://www.youtube.com/watch?v=Xf5o13Zutak",
+    description:
+      "Real-time school bus tracking system using a low-powered GPS tracking device, with a web app for schools to monitor their school buses, and mobile app for parents to know their child’s location.",
+    stack: [
+      "React",
+      "Flutter",
+      "Firebase",
+      "Open Street Map",
+      "Lora",
+      "Arduino, NodeMCU",
+    ],
+  },
+  {
+    image: maitrika,
+    title: "Maitrika",
+    category: "",
+    link: "https://github.com/Yunika-Bajracharya/Hungry-Hackee-Hunts",
+    demo: "",
+    description:
+      " Application that connects health workers with doctors, tracks events, and generates reports based on data taken by health workers to aid in checkups.",
+    stack: ["MERN stack", "WebRTC", "i18next", "chart.js"],
+  },
+  {
+    image: hhh,
+    title: "Hungry Hackee Hunts",
+    category: "",
+    link: "https://github.com/Yunika-Bajracharya/Hungry-Hackee-Hunts",
+    demo: "https://www.yunikabajracharya.com.np/Hungry-Hackee-Hunts/",
+    description:
+      "A game in which you help Hackee, the squirrel, in her walnut heist while visualizing different sorting algorithms and a maze generator created using recursive depth first search. There are two levels.",
+    stack: ["JavaScript", "HTML", "CSS", "Canva"],
+  },
+  {
+    image: chess,
+    title: "Chess",
+    category: "",
+    link: "https://github.com/Yunika-Bajracharya/Chess",
+    demo: "",
+    description:
+      "A chess game with features such as timer, single/multi-player mode, scoreboard, and resign options, created using C++ and SDL2 for GUI.",
+    stack: ["C++", "SDL2", "CMAKE"],
+  },
+  {
+    image: clhp,
+    title: "Code Like Her Fellowship Projects",
+    category: "",
+    link: "https://github.com/Coderushnepal/YunikaBajracharya",
+    demo: "",
+    description:
+      "Here is the list of projects and assignments I did during the fellowship.",
+    stack: ["HTML", "CSS", "JavaScript", "Express"],
+  },
+  {
+    image: dhungedhara,
+    title: "Dhungedhara",
+    category: "",
+    link: "https://github.com/Yunika-Bajracharya/Dhungedhara",
+    demo: "",
+    description:
+      "This is a simulation of a traditional stone drinking water fountain called Lho Hiti. The 3D model also includes the surrounding, flowers, stairs and water jar called 'gagri' in Nepali with the day time as well as night time views.",
+    stack: ["C++", "OpenGL", "GLSL", "Blender"],
+  },
+  {
+    image: sathi,
+    title: "Sathi",
+    category: "",
+    link: "",
+    demo: "https://sathi-app.github.io/",
+    description: (
+      <ul>
+        <li>
+          Sathi is a friendly app for Type I Diabetic Patients to make their
+          life easier. It sends notification to the patient regarding timely
+          blood sugar checkup and medication, to the trusted person if the
+          patient misses the notification, and to the doctor if the patient's
+          blood sugar level goes extremely high.
+        </li>
+        <br />
+        <li>
+          {" "}
+          Project Details:
+          <a
+            href="https://www.youtube.com/watch?v=mxnGCWSWLTY&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4"
+            target="_blank"
+          >
+            {" "}
+            Intro Video{" "}
+          </a>
+          &#x2B29;
+          <a
+            href="https://www.youtube.com/watch?v=ij6_imqs3Nc&list=PLfEevmzbs5mMKyBsHwh8c73v6xC60i4eX&index=4"
+            target="_blank"
+          >
+            {" "}
+            Demo Video
+          </a>
+        </li>
+      </ul>
+    ),
+    stack: ["Seeds For the Future 2021 Project"],
+  },
+];
